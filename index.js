67e83407caf90c7bf6614a6dd150c555800c12f2
Jaedson <jaedson33@gmail.com>
2021-10-18 22:39:55 -0300

    Made super simplification of the project.

---
+++ index.js
@@ -0,0 +1,361 @@
+'use strict'
+
+import boxxy14b from './fonts/Boxxy/14-b'
+import boxxy14 from './fonts/Boxxy/14'
+
+import cherry10b from './fonts/Cherry/10-b'
+import cherry10 from './fonts/Cherry/10'
+import cherry11b from './fonts/Cherry/11-b'
+import cherry11 from './fonts/Cherry/11'
+import cherry13b from './fonts/Cherry/13-b'
+import cherry13 from './fonts/Cherry/13'
+
+import ctrld10b from './fonts/Ctrld/10-b'
+import ctrld10 from './fonts/Ctrld/10'
+import ctrld13b from './fonts/Ctrld/13-b'
+import ctrld13 from './fonts/Ctrld/13'
+import ctrld16b from './fonts/Ctrld/16-b'
+import ctrld16 from './fonts/Ctrld/16'
+
+import dina10b from './fonts/Dina/10-b'
+import dina10 from './fonts/Dina/10'
+import dina12b from './fonts/Dina/12-b'
+import dina12 from './fonts/Dina/12'
+import dina13b from './fonts/Dina/13-b'
+import dina13 from './fonts/Dina/13'
+
+import dylex10b from './fonts/Dylex/10-b'
+import dylex10 from './fonts/Dylex/10'
+import dylex13b from './fonts/Dylex/13-b'
+import dylex13 from './fonts/Dylex/13'
+import dylex20b from './fonts/Dylex/20-b'
+import dylex20 from './fonts/Dylex/20'
+
+import gohufont11b from './fonts/Gohufont/11-b'
+import gohufont11 from './fonts/Gohufont/11'
+import gohufont14b from './fonts/Gohufont/14-b'
+import gohufont14 from './fonts/Gohufont/14'
+
+import kakwa12b from './fonts/Kakwa/12-b'
+import kakwa12 from './fonts/Kakwa/12'
+
+import lokaltog10b from './fonts/Lokaltog/10-b'
+import lokaltog10 from './fonts/Lokaltog/10'
+import lokaltog12b from './fonts/Lokaltog/12-b'
+import lokaltog12 from './fonts/Lokaltog/12'
+
+import mplus10b from './fonts/MPlus/10-b'
+import mplus10 from './fonts/MPlus/10'
+import mplus12b from './fonts/MPlus/12-b'
+import mplus12 from './fonts/MPlus/12'
+
+import orp12b from './fonts/Orp/12-b'
+import orp12 from './fonts/Orp/12'
+
+import scientifica11b from './fonts/Scientifica/11-b'
+import scientifica11 from './fonts/Scientifica/11'
+
+import sq15b from './fonts/Sq/15-b'
+import sq15 from './fonts/Sq/15'
+
+import terminus14b from './fonts/Terminus/14-b'
+import terminus14 from './fonts/Terminus/14'
+import terminus16b from './fonts/Terminus/16-b'
+import terminus16 from './fonts/Terminus/16'
+import terminus18b from './fonts/Terminus/18-b'
+import terminus18 from './fonts/Terminus/18'
+import terminus20b from './fonts/Terminus/20-b'
+import terminus20 from './fonts/Terminus/20'
+import terminus22b from './fonts/Terminus/22-b'
+import terminus22 from './fonts/Terminus/22'
+import terminus24b from './fonts/Terminus/24-b'
+import terminus24 from './fonts/Terminus/24'
+import terminus28b from './fonts/Terminus/28-b'
+import terminus28 from './fonts/Terminus/28'
+import terminus32b from './fonts/Terminus/32-b'
+import terminus32 from './fonts/Terminus/32'
+
+import tewi11b from './fonts/Tewi/11-b'
+import tewi11 from './fonts/Tewi/11'
+
+import triskweline13b from './fonts/Triskweline/13-b'
+import triskweline13 from './fonts/Triskweline/13'
+
+export const Fonts = {
+  Boxxy: {
+    [14]: {
+      regular: boxxy14,
+      bold: boxxy14b,
+    },
+  },
+  Cherry: {
+    [10]: {
+      regular: cherry10,
+      bold: cherry10b,
+    },
+    [11]: {
+      regular: cherry11,
+      bold: cherry11b,
+    },
+    [13]: {
+      regular: cherry13,
+      bold: cherry13b,
+    },
+  },
+  Ctrld: {
+    [10]: {
+      regular: ctrld10,
+      bold: ctrld10b,
+    },
+    [13]: {
+      regular: ctrld13,
+      bold: ctrld13b,
+    },
+    [16]: {
+      regular: ctrld16,
+      bold: ctrld16b,
+    },
+  },
+  Dina: {
+    [10]: {
+      regular: dina10,
+      bold: dina10b,
+    },
+    [12]: {
+      regular: dina12,
+      bold: dina12b,
+    },
+    [13]: {
+      regular: dina13,
+      bold: dina13b,
+    },
+  },
+  Dylex: {
+    [10]: {
+      regular: dylex10,
+      bold: dylex10b,
+    },
+    [13]: {
+      regular: dylex13,
+      bold: dylex13b,
+    },
+    [20]: {
+      regular: dylex20,
+      bold: dylex20b,
+    },
+  },
+  Gohufont: {
+    [11]: {
+      regular: gohufont11,
+      bold: gohufont11b,
+    },
+    [14]: {
+      regular: gohufont14,
+      bold: gohufont14b,
+    },
+  },
+  Kakwa: {
+    [12]: {
+      regular: kakwa12,
+      bold: kakwa12b,
+    },
+  },
+  Lokaltog: {
+    [10]: {
+      regular: lokaltog10,
+      bold: lokaltog10b,
+    },
+    [12]: {
+      regular: lokaltog12,
+      bold: lokaltog12b,
+    },
+  },
+  MPlus: {
+    [10]: {
+      regular: mplus10,
+      bold: mplus10b,
+    },
+    [12]: {
+      regular: mplus12,
+      bold: mplus12b,
+    },
+  },
+  Orp: {
+    [12]: {
+      regular: orp12,
+      bold: orp12b,
+    },
+  },
+  Scientifica: {
+    [11]: {
+      regular: scientifica11,
+      bold: scientifica11b,
+    },
+  },
+  Sq: {
+    [15]: {
+      regular: sq15,
+      bold: sq15b,
+    },
+  },
+  Terminus: {
+    [14]: {
+      regular: terminus14,
+      bold: terminus14b,
+    },
+    [16]: {
+      regular: terminus16,
+      bold: terminus16b,
+    },
+    [18]: {
+      regular: terminus18,
+      bold: terminus18b,
+    },
+    [20]: {
+      regular: terminus20,
+      bold: terminus20b,
+    },
+    [22]: {
+      regular: terminus22,
+      bold: terminus22b,
+    },
+    [24]: {
+      regular: terminus24,
+      bold: terminus24b,
+    },
+    [28]: {
+      regular: terminus28,
+      bold: terminus28b,
+    },
+    [32]: {
+      regular: terminus32,
+      bold: terminus32b,
+    },
+  },
+  Tewi: {
+    [11]: {
+      regular: tewi11,
+      bold: tewi11b,
+    },
+  },
+  Triskweline: {
+    [13]: {
+      regular: triskweline13,
+      bold: triskweline13b,
+    },
+  },
+}
+
+/**
+ *
+ * @param {{ DWIDTH: number, BBh: number, BBox: number, BBoy: number, BITMAP: number[], BITS: number }[]} font
+ * @param {number} lineHeight
+ * @param {1 | 2} scale
+ * @param {CanvasRenderingContext2D} context
+ * @param {string} text
+ * @param {number} x
+ * @param {number} y
+ * @param {number} maxWidth
+ * @param {'left' | 'center' | 'right'} align
+ */
+export function Write(
+  font,
+  lineHeight,
+  scale,
+  context,
+  text,
+  x,
+  y,
+  maxWidth,
+  align
+) {
+  /** @param {number} code */
+  function getGlyph(code) {
+    if (code > 127) code -= 161
+    if (code < 32) code = 32
+    const g = font[code - 32]
+    return g
+  }
+
+  /**
+   *
+   * @param {number} c
+   * @param {number} bx
+   * @param {number} by
+   * @returns {number}
+   */
+  function drawChar(c, bx, by) {
+    const g = getGlyph(c)
+    const b = g['BITMAP']
+    const ox = bx + g['BBox'] * scale - 1
+    const oy = by - g['BBoy'] * scale - g['BBh'] * scale + 1
+    for (let y = 0, len = b.length; y < len; y++) {
+      const l = b[y]
+      for (let i = g.BITS, x = 0; i >= 0; i--, x++) {
+        if (((l >> i) & 0x01) == 1) {
+          context.fillRect(ox + x * scale, oy + y * scale, scale, scale)
+        }
+      }
+    }
+    return bx + g['DWIDTH'] * scale
+  }
+
+  /**
+   *
+   * @param {string} text
+   * @param {number} x
+   * @param {number} y
+   */
+  function writeLine(text, x, y) {
+    for (let i = 0, len = text.length; i < len; i++) {
+      const c = text[i].charCodeAt(0)
+      x = drawChar(c, x, y)
+    }
+  }
+
+  /**
+   *
+   * @param {string} text
+   * @returns {number}
+   */
+  function measureText(text) {
+    let width = 0
+    for (let i = 0, len = text.length; i < len; i++) {
+      const c = text[i].charCodeAt(0)
+      const g = getGlyph(c)
+      width += g['DWIDTH'] * scale
+    }
+    return width
+  }
+
+  // Alocate space for the first line
+  y += lineHeight * scale
+
+  // Remove all non-ASCII characters.
+  text = text.replace(/[^\x00-\xFF]/g, '')
+
+  const words = text.split(' ')
+  let line = ''
+  let lineWidth = 0
+  function getX() {
+    if (align == 'left') return x
+    else {
+      let freeSpace = maxWidth - lineWidth
+      if (align == 'right') return x + freeSpace
+      else return x + Math.floor(freeSpace / 2)
+    }
+  }
+  for (let n = 0; n < words.length; n++) {
+    const testLine = line + words[n] + ' '
+    const testLineWidth = measureText(testLine).width
+    if (testLineWidth > maxWidth && n > 0) {
+      writeLine(line, getX(), y)
+      line = words[n] + ' '
+      y += lineHeight * scale
+    } else line = testLine
+    lineWidth = testLineWidth
+  }
+  lineWidth = measureText(line).width
+  writeLine(line, getX(), y)
+  return y
+}
--- lib/index.ts
+++
@@ -1,2 +0,0 @@
-export { Fonts } from './fonts'
-export { Writer } from './writer'
